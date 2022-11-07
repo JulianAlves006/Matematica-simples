8c17afdd421eb55ac9063dd761d73afdacf5e7cf
Julian Alves <116108866+JulianAlves006@users.noreply.github.com>
2022-11-07 15:52:22 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int n;
+    printf("Pense em um numero\n");
+    sleep(5);
+
+    printf("Agora some este numero com 10\n");
+    sleep(5);
+
+    printf("Some mais 30\n");
+    sleep(5);
+
+    printf("Agora tire o numero que voce pensou\n");
+    sleep(5);
+
+    printf("O resultado foi 40?");
+}
